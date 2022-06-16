ea2dd678595248daba58332de9551ff7bf3c0661
marinaitstep <marinaitstep@gmail.com>
2022-06-17 01:36:20 +0300

    flex navs and scroll

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,21 @@
 
 function Footer(){
+
     function refresh() {
         window.location.reload()
       }
-    function ScrollTop(){
-        let rootElement = document.documentElement
-        rootElement.scrollTo({
+      const ScrollUp = () => {
+        window.scrollTo({
             top: 0,
-            behavior: "smooth"
-          })
-    }
+            left: 0,
+            behavior: 'smooth',
+          });
+      }
     return(
         <>
         <center><button id="butref" onClick={refresh}>обнулитися</button> 
         <a id="author" href="https://www.instagram.com/alisafairytale/">автор</a> 
-        <div onClick={ScrollTop} id="scrollToTopBtn">UP</div>
+        <div onClick={ScrollUp} id="scrollToTopBtn">UP</div>
         </center> 
         
         </>
